9c7b9ae1c68588851ef02ef41ad9b6967ca9ba78
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-02 20:38:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,8 @@ class Application
       if item = @@items.find { |i| i.name == item_name }
         resp.write item.price
       else
-        resp.status = 400
         resp.write "Item not found"
-
+        resp.status = 400
       end
     else
       resp.write "Route not found"
