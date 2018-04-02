5f7e06d12b02445b2b51054345a37ea4cc62313e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-02 20:43:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,9 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items").last
-      if item = @@items.find{ |i| i.name == item_name }
-        resp.write "#{item.price}"
+      item_name = req.path.split("/items/").last
+      if item = @@items.find { |i| i.name == item_name }
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
